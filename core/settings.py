babfa04d9724e02799280a550746a8be4b1d7c9f
SauloAndres1982 <saulo.carmona@soyrosarista.edu.co>
2023-10-16 19:57:17 -0500

    Integrando Django con React

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -130,7 +130,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/" 
 
-STATICFILE_DIRS = [os.path.join(BASE_DIR, 'build/static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')] 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
