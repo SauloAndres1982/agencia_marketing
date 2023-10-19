6ec4cc8fac7521d5d89808734978f705aee14393
SauloAndres1982 <saulo.carmona@soyrosarista.edu.co>
2023-10-19 01:30:14 -0500

    commit con uso react

---
+++ src/components/navigation/Footer.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux"
+
+function Footer() {
+    return(
+        <footer>
+            Footer
+        </footer>
+    )
+}
+const mapStateToProps=state=>({
+})
+
+export default connect(mapStateToProps, {
+
+}) (Footer)
